60adc8703e6a48d82eff201680c904d06a08f9b8
Richard <richard232manansala@gmail.com>
2023-05-27 18:22:50 +0800

    Made bullet collide with default colliders such as walls

--- World/Weapons/Pistol/Bullet.cs
+++ World/Weapons/Pistol/Bullet.cs
@@ -5,15 +5,18 @@ using Shuut.Scripts.Hurtbox;
 
 public partial class Bullet : Node2D
 {
+    [Export(PropertyHint.Layers2DPhysics)] private uint colliderMask;
     [Export] private float Speed = 1000;
     [Export] private Hitbox _hitbox;
     private Vector2 direction;
     public int damage;
+    private PhysicsDirectSpaceState2D space;
 
     public override void _Ready()
     {
         base._Ready();
         _hitbox.OnHitboxHit += HitboxOnOnHitboxHit;
+        this.space = GetWorld2D().DirectSpaceState;
     }
 
     private void HitboxOnOnHitboxHit(Hurtbox hurtbox)
@@ -35,5 +38,22 @@ public partial class Bullet : Node2D
     {
         base._PhysicsProcess(delta);
         GlobalPosition += direction * Speed * (float)delta;
+
+        var query = new PhysicsShapeQueryParameters2D()
+        {
+            Shape = _hitbox.CollisionShape2D.Shape,
+            CollisionMask = _hitbox.CollisionMask & colliderMask,
+            Transform = new Transform2D()
+            {
+                Origin = GlobalPosition
+            }
+        };
+
+        var res = space.IntersectShape(query);
+        if (res.Count > 0)
+        {
+            QueueFree();
+        }
+
     }
 }

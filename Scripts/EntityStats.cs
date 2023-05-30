a6484693d017eb3d9dbc5dc780c644c718cc3443
Richard <richard232manansala@gmail.com>
2023-05-30 10:00:00 +0800

    Added an EntityStats Resource file and used it to zombie & player

---
+++ Scripts/EntityStats.cs
@@ -0,0 +1,17 @@
+﻿using Godot;
+using MonoCustomResourceRegistry;
+
+namespace Shuut.Scripts;
+
+
+[RegisteredType(nameof(EntityStats))]
+public partial class EntityStats : Resource
+{
+    [Export] private float _tileMovementSpeed = 1;
+    [Export] public float StartingHealth { get; set; } = 100;
+    
+    [Export] public int BaseDamage = 1;
+
+    public float MovementSpeed => _tileMovementSpeed * Constants.Tile.Size;
+
+}
--- Scripts/IDamager.cs
+++ Scripts/IDamager.cs
@@ -4,6 +4,6 @@ namespace Shuut.Scripts;
 
 public interface IDamager
 {
-    public int BaseDamage { get; set; }
+    public int BaseDamage { get; }
     public Vector2 GlobalPosition { get; set; }
 }
